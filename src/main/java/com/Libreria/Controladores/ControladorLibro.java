44501152501aaf4ebce78d6d1f2ffe3e979785d5
VieDev <piri.viejo@gmail.com>
2021-10-10 17:31:06 -0300

    Configuro eliminar Libro en el controlador

--- src/main/java/com/Libreria/Controladores/ControladorLibro.java
+++ src/main/java/com/Libreria/Controladores/ControladorLibro.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -65,4 +66,12 @@ public class ControladorLibro {
 			return registro(model);
 		}
 	}
+	
+	@GetMapping("/eliminar/{id}")
+	public String eliminar(@PathVariable int id) {
+		serLib.eliminarLibro(id);
+		return "redirect:/libro/lista";
+	}
+	
+	
 }
