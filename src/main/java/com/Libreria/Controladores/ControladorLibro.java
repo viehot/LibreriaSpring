e6951cf0b49c8978e04dcc4b8cf2bd7db166b1e8
VieDev <piri.viejo@gmail.com>
2021-10-09 23:55:24 -0300

    Agregamos controlador y vista de listar Libro

--- src/main/java/com/Libreria/Controladores/ControladorLibro.java
+++ src/main/java/com/Libreria/Controladores/ControladorLibro.java
@@ -1,5 +1,28 @@
 package com.Libreria.Controladores;
 
-public class ControladorLibro {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.Libreria.Servicios.ServicioLibro;
+import com.Libreria.entidades.Libro;
+
+@Controller
+@RequestMapping("/libro")
+public class ControladorLibro {
+	
+	@Autowired
+	private ServicioLibro serLib;
+	
+	@GetMapping("/lista")
+	public String lista(ModelMap model) {
+		model.put("lista", "Lista de Libros");
+		List<Libro> lib = serLib.listarLibros();
+		model.addAttribute("libro", lib);
+		return "/libro/listarLibro";
+	}
 }
