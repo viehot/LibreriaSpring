524d64bc83881796e8fdde89853946b7fdddc1ad
VieDev <piri.viejo@gmail.com>
2021-10-17 18:06:32 -0300

    Configuramos la encryptacion del password

--- src/main/java/com/Libreria/security/ConfiguracionSeguridad.java
+++ src/main/java/com/Libreria/security/ConfiguracionSeguridad.java
@@ -1,15 +1,28 @@
 package com.Libreria.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.Libreria.Servicios.ServicioUsuario;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled=true)
 public class ConfiguracionSeguridad extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	private ServicioUsuario serviUser;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(serviUser).passwordEncoder(new BCryptPasswordEncoder());
+	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
