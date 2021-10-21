a08fc323b49ec953ec99843bc45103423bdafdf1
VieDev <piri.viejo@gmail.com>
2021-10-21 02:07:56 -0300

    Configure registro de usuario y admin

--- src/main/java/com/Libreria/Controladores/ControladorMain.java
+++ src/main/java/com/Libreria/Controladores/ControladorMain.java
@@ -1,15 +1,23 @@
 package com.Libreria.Controladores;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.Libreria.Servicios.ServicioUsuario;
 
 
 
 @Controller
 @RequestMapping("/") //Primera vista - Siempre se indica así // Es la posición de partida
 public class ControladorMain {
+	
+	@Autowired
+	private ServicioUsuario servicioUsuario;
 	
 	@GetMapping("/")
 	public String index() {
@@ -18,7 +26,7 @@ public class ControladorMain {
 	
 	@GetMapping("/registro/usuario")
 	public String registroUser() {
-		return "registro";
+		return "registro.html";
 	}
 	
 	@GetMapping("/registro/admin")
@@ -27,12 +35,14 @@ public class ControladorMain {
 	}
 	
 	@PostMapping("/registro/usuario")
-	public String regUser() {
+	public String regUser(ModelMap model,@RequestParam String nombre,@RequestParam String apellido,@RequestParam String telefono,@RequestParam String email,@RequestParam String password) {
+		servicioUsuario.registroUsuario(nombre, apellido, telefono, email, password);
 		return "redirect:/";
 	}
 	
 	@PostMapping("/registro/admin")
-	public String regAdmin() {
+	public String regAdmin(ModelMap model,@RequestParam String nombre,@RequestParam String apellido,@RequestParam String telefono,@RequestParam String email,@RequestParam String password) {
+		servicioUsuario.registroAdmin(nombre, apellido, telefono, email, password);
 		return "redirect:/";
 	}
 
